ea590e90ab751bfd2bacba3a899b95b851590120
maria-lysyk <113712306+maria-lysyk@users.noreply.github.com>
2023-03-06 14:02:10 +0200

    Add files via upload

---
+++ About page/masters.js
@@ -0,0 +1,20 @@
+export default[
+  {
+    "image": "./images/w-1.png",
+    "name": "Liuda",
+    "position": "Stylist",    
+    "info": "Liuda is a new member of our team trained to the highest standards by Tanya. She is thoughtful, motivated and has a truly special touch in all areas of cut and coloring."
+    },
+    {
+    "image": "./images/w-2.png",
+    "name": "Yulia",
+    "position": "Brow Artist",
+    "info": "Yuliya is passionate about her art andviews eyebrow treatment as a livemasterpiece. As a perfectionist by nature, she has a keen eye for design and passion for enhancing natural beauty."
+    },
+    {
+    "image": "./images/w-3.png",
+    "name": "Olga",
+    "position": "Receptionist",
+    "info": "As the first and last face our clients see, I love making genuine connections, and seeing the amazing transitions our clients go through during their service."
+    }
+  ]
